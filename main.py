959ce9949cc2d0cd79dffa9c5f9c44a3de1a0977
Suah-Cho <sacho0929@gmail.com>
2024-09-03 11:02:29 +0900

    main.py 수정

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
+import signal
 import subprocess
 from azure.iot.device import IoTHubDeviceClient
 import os
-import json
 import logging
 
 # 디바이스 연결 문자열 가져오기
 conn_str = os.getenv('DEVICE_CONNECT_STRING')
-logging.info("Connection string: ", conn_str)
+logging.info(f"Device connection string: {conn_str}")
 device_client = IoTHubDeviceClient.create_from_connection_string(conn_str)
 
 # 디바이스 연결
 device_client.connect()
 
-def execute_command(command):
+def command(command):
     try:
-        # 명령어 실행
-        logging.info(f"Executing command: {command}")
-        result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        logging.info("Command output:", result.stdout)
+        logging.info(f"Excuting command: {command}")
+        result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        logging.info(f"Command executed successfully", result.stdout)
         if result.stderr:
-            logging.info("Command error output:", result.stderr)
+            logging.error(f"Command Error output: {result.stderr}")
     except subprocess.CalledProcessError as e:
-        logging.info(f"An error occurred: {e}")
+        logging.error(f"Error: {e}")
 
 # 메시지 수신 대기
 logging.info("Waiting for messages... Press Ctrl+C to exit")
@@ -29,14 +28,22 @@ while True:
     try:
         message = device_client.receive_message()
         logging.info("Message received: ", message.data)
-        
-        # 수신된 메시지를 명령어로 실행
-        command = message.data.decode("utf-8")
-        execute_command(command)
+
+        # 수신된 메세지를 기반으로 업데이트 작업 실행
+        com = message.data['command']
+        command(command=com)
     except KeyboardInterrupt:
         # 디바이스 연결 해제
         logging.info("Disconnecting from IoT Hub")
         device_client.disconnect()
         break
     except Exception as e:
-        logging.info(f"An error occurred: {e}")
+        logging.error(f"An error occurred: {e}")
+
+
+# def test(command):
+#     result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+#     print("Command output:", result.stdout)
+
+
+# test("ls -al")
